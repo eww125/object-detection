ba6349a49c6df4b938f8051b4e2871a91d6acf06
Evan Wappel <evanwappel@evans-mbp.lan>
2020-07-07 16:01:03 -0700

    sync

--- README.md
+++ README.md
@@ -6,3 +6,20 @@
 ## Gather Images, split train/test = 80/20 or 90/10
 ### `python transform_image_resolution.py -d images/train/ -s 800 600`
 ### `python transform_image_resolution.py -d images/test/ -s 800 600`
+## ImgLabel
+### `python xml_to_csv.py`
+### This creates two files in the images directory:
+#### test_labels.csv
+#### train_labels.csv
+### Before we can transform the newly created files to TFRecords we need to change a few lines in the generate_tfrecords.py file.
+### `def class_text_to_int(row_label):`
+### `    if row_label == 'Raspberry_Pi_3':`
+### `        return 1`
+### `    elif row_label == 'Arduino_Nano':`
+### `        return 2`
+### `    elif row_label == 'ESP8266':`
+### `        return 3`
+### `    elif row_label == 'Heltec_ESP32_Lora':`
+### `        return 4`
+### `    else:`
+### `    return None`
--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -29,10 +29,8 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'Steel_Pole':
+    if row_label == 'raccoon':
         return 1
-    elif row_label == 'Wood_Pole':
-        return 2
     else:
         None
 
@@ -44,7 +42,7 @@ def split(df, group):
 
 
 def create_tf_example(group, path):
-    with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
@@ -85,7 +83,7 @@ def create_tf_example(group, path):
 
 
 def main(_):
-    writer = tf.io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
@@ -99,4 +97,4 @@ def main(_):
 
 
 if __name__ == '__main__':
-    tf.compat.v1.app.run()
+    tf.app.run()
